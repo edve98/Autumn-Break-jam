bdaddc43ec9c7077d77fcc5ff9014cdae3c93a05
edve98 <edvinas.senda@gmail.com>
2016-10-30 13:04:58 +0200

    Adding login barebones

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,5 +1,6 @@
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
+import { Meteor } from 'meteor/meteor';
 
 import '/imports/ui/layouts/main-layout.js';
 import '/imports/ui/pages/login.js';
@@ -18,8 +19,15 @@ FlowRouter.route('/login', {
 	}
 });
 
+FlowRouter.route('/logout', {
+	action: function() {
+		Meteor.logout();
+		FlowRouter.go('/login');
+	}
+});
+
 FlowRouter.notFound = {
-  action: function() {
-    BlazeLayout.render('Main_layout', { content: 'Not_found' });
-  },
+	action: function() {
+		BlazeLayout.render('Main_layout', { content: 'Not_found' });
+	},
 };
--- imports/ui/pages/login.js
+++ imports/ui/pages/login.js
@@ -1,1 +1,28 @@
-import './login.html'
+import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+import './login.html';
+
+
+Template.Login.events({
+	'submit .login': function(event){
+		event.preventDefault();
+		var emailVar = event.target.loginEmail.value;
+		var passwordVar = event.target.loginPassword.value;
+		
+		Meteor.loginWithPassword(emailVar, passwordVar);
+		FlowRouter.go('/');
+	},
+	'submit .register': function(event) {
+		event.preventDefault();
+		var emailVar = event.target.registerEmail.value;
+		var passwordVar = event.target.registerPassword.value;
+		
+		Accounts.createUser({
+			email: emailVar,
+			password: passwordVar
+		});
+		FlowRouter.go('/');
+	}
+});
